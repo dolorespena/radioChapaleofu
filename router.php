dd009be0efdf812521a634dc0afddab697081081
Dolores Pena <dolorespenadecristofaro@gmail.com>
2020-07-29 18:08:56 -0300

    .gitignore, .htaccess e inicio de pag

---
+++ router.php
@@ -0,0 +1,19 @@
+<?php
+
+    // definimos la base url de forma dinamica
+    define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+    // define una acción por defecto
+    if (empty($_GET['action'])) {
+        $_GET['action'] = 'columnistas';
+    } 
+
+    // toma la acción que viene del usuario y parsea los parámetros
+    $accion = $_GET['action']; 
+    $parametros = explode('/', $accion);
+
+    // decide que camino tomar según TABLA DE RUTEO
+    switch ($parametros[0]) {
+
+
+    }
